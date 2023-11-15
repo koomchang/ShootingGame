8f0d4efb4f3cbe3c69ef5411e37f3cbdd837e093
koomchang <leahpar0401@gmail.com>
2023-11-15 16:26:06 +0900

    feat: 적이 30퍼센트 확률로 플레이어 방향, 나머지 확률로 아래 방향으로 나오도록 설정

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 	public float speed = 5;
+	Vector3 dir;
+
+	private void Start() {
+		int randValue = UnityEngine.Random.Range(0, 10);
+		if (randValue < 3) {
+			GameObject target = GameObject.Find("Player");
+			dir = target.transform.position - transform.position;
+			dir.Normalize();
+		}
+		else {
+			dir = Vector3.down;
+		}
+	}
 
 	private void Update() {
-		Vector3 dir = Vector3.down;
 		transform.position += dir * (speed * Time.deltaTime);
 	}
 
