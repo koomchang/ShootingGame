60c745b82b6beba59c264cb7ba524e490513110c
koomchang <leahpar0401@gmail.com>
2023-11-15 16:05:56 +0900

    feat: 플레이어가 총알을 발사할 수 있도록 PlayerFire 구현

---
+++ Assets/Scripts/PlayerFire.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFire : MonoBehaviour {
+	public GameObject bulletFactory;
+	public GameObject firePosition;
+
+	private void Update() {
+		if (Input.GetButtonDown("Fire1")) {
+			GameObject bullet = Instantiate(bulletFactory);
+			bullet.transform.position = firePosition.transform.position;
+		}
+	}
+}
